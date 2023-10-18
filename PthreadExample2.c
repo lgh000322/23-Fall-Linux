58874c05c93a0e3a81f5d2bc1385b8e194cb962e
Lee GyeongHun <kang000322@gmail.com>
2023-10-18 16:31:20 +0900

    pthreadexample2 and 3

---
+++ PthreadExample2.c
@@ -0,0 +1,39 @@
+#include <pthread.h>
+#include <stdio.h>
+#include <stdlib.h>
+int acc=0;
+
+void *TaskCode(void *argument)
+{
+	int tid;
+	tid=*((int*)argument);
+	for(int i=0;i<=1000000;i++)
+	{
+
+		acc+=1;
+	}
+	return NULL;
+}
+
+int main(int argc,char* argv[])
+{
+	pthread_t threads[4];
+	int args[4];
+	int i;
+	
+	for(i=0;i<4;i++)
+	{
+		args[i]=i;
+		pthread_create(&threads[i],NULL,TaskCode,(void*)&args[i]);
+	}
+
+	for(int i=0;i<4;i++)
+	{
+		pthread_join(threads[i],NULL);
+	}
+
+	
+	printf("%d\n",acc);
+
+	return 0;
+}
